e212e3a4457a7f0b0ce1fe566f9b489a7bea68a9
a1paka <50889219+a1paka@users.noreply.github.com>
2019-11-01 22:43:50 +0300

    предфинальн правки

---
+++ js/modal-window-min.js
@@ -0,0 +1,1 @@
+var link=document.querySelector(".search-hotels-sedona");var popup=document.querySelector(".modal-window");popup.classList.add("modal-window-show");link.addEventListener("click",function(evt){evt.preventDefault();popup.classList.toggle("modal-window-show")});
