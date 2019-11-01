6f10c156dae064eac996c487b423013f8883da60
a1paka <50889219+a1paka@users.noreply.github.com>
2019-11-01 19:43:03 +0300

    Анимация модалки

--- js/modal-window.js
+++ js/modal-window.js
@@ -2,6 +2,7 @@
     var link = document.querySelector(".search-hotels-sedona");
     var popup = document.querySelector(".modal-window");
 
+    popup.classList.add("modal-window-show");
     link.addEventListener("click", function (evt) {
     evt.preventDefault(); 
     popup.classList.toggle("modal-window-show");
